00c18308baff449bc9bcfe66dc483b5519c5b24f
Victor Costa Milhomem <victorcosta.prog@gmail.com>
2025-06-09 12:22:38 -0300

    resolvendo a tarefa1

---
+++ Modulo2/ValidacaodeDatas.js
@@ -0,0 +1,9 @@
+// Seção 1: Estruturas de Controle Avançadas
+
+function ehDataValida(dia, mes, ano) {
+  if (ano < 1 || mes < 1 || mes > 12 || dia < 1) return false;
+
+  const diasPorMes = [31, (ano % 4 === 0 && (ano % 100 !== 0 || ano % 400 === 0)) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+  return dia <= diasPorMes[mes - 1];
+}
